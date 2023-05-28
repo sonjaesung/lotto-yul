867bcde4fd776f56bfcf81c5a1ebe49cc3f56abb
lokises <lokises@mcircle.biz>
2023-05-28 22:30:03 +0900

    메인 페이지 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home = observer(() => {
   }, []);
 
   return (
-    <div className="lotto_info_area">
+    <div className="home_area">
       <div className="title">Eun Yul's Lotto</div>
       <div className="workspace">
         <div className="sub_title">기술 스펙</div>
@@ -24,12 +24,12 @@ const Home = observer(() => {
         <div className="sub_title">작업 환경</div>
         <div className="des">npm Version : v8.1.2</div>
         <div className="des">node Version : v16.13.2</div>
-        <div className="des">git : v16.13.2</div>
+        <div className="des">git : https://github.com/sonjaesung/lotto-yul</div>
       </div>
       <div className="introduce">
         <div className="sub_title">작업자 정보</div>
-        <div className="des">npm Version : v8.1.2</div>
-        <div className="des">node Version : v16.13.2</div>
+        <div className="des">작업자 : Eun Yul</div>
+        <div className="des">E-Mail : loki92@naver.com</div>
       </div>
     </div>
   );
